b35eafbc844f146a9e3534c70c1333afb58f7e9c
BYADIR-IBRAHIM-A <BYADER144110@GMAIL.com>
2025-03-09 11:21:51 +0300

    CHANGE CODES

--- bookstore.js
+++ bookstore.js
@@ -1,6 +1,14 @@
+let userInput = prompt("يرجى إدخال قيمة:");
+console.log("القيمة التي أدخلها المستخدم هي:", userInput);
+/*
 // تعريف المصفوفة لتخزين معلومات الكتب
-const books = [];
-
+let books = [
+    [1, "Start with Why", "Simon Sinek", 80.0, 13],
+    [2, "But How Do It Know", "J. Clark Scott", 59.9, 22],
+    [3, "Clean Code", "Robert Cecil Martin", 50.0, 5],
+    [4, "Zero to One", "Peter Thiel", 45.0, 12],
+    [5, "You Don't Know JS", "Kyle Simpson", 39.9, 9]
+];
 // دالة لإضافة كتاب جديد
 function addBook(bookId, bookTitle, author, price, quantity) {
     books.push([bookId, bookTitle, author, price, quantity]);
@@ -44,7 +52,7 @@ addBook(4, "Zero to One", "Peter Thiel", 45.0, 12);
 addBook(5, "You don't know JS", "Kyle Simpson", 39.9, 9);
 
 console.log(searchByBookId(3)); // [3, "Clean Code", "Robert Cecil Martin", 50.0, 5]
-console.log(searchByTitle("Zero to One")); // [4, "Zero to One", "Peter Thiel", 45.0, 12]
+console.log(searchByTitle("Zero to")); // [4, "Zero to One", "Peter Thiel", 45.0, 12]
 console.log(searchByAuthor("Kyle Simpson")); // [5, "You don't know JS", "Kyle Simpson", 39.9, 9]
 
 // دالة لبيع كتاب وإصدار فاتورة
@@ -72,3 +80,4 @@ function sellBook(bookTitle, quantity, balance) {
 console.log(sellBook("Zero to One", 3, 200)); // مثال لنجاح عملية البيع
 console.log(sellBook("Clean Code", 10, 500)); // مثال لفشل عملية البيع بسبب عدم توفر الكمية المطلوبة
 console.log(sellBook("But how do it know", 5, 100)); // مثال لفشل عملية البيع بسبب الرصيد غير كافي
+*/
